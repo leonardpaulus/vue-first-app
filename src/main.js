c845bdc108d796fe90f0bfe064fcbd226e501db7
leonardpaulus <leonard.paulus@agenturignis.de>
2022-02-16 08:41:57 +0100

    ✨ Add form

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import FriendContact from "./components/FriendContact.vue";
-import NewFriend from "./components/NewFriend.vue";
+import { createApp } from 'vue';
 
-const app = createApp(App);
+import App from './App.vue';
 
-app.component("friend-contact", FriendContact);
-app.component("new-friend", NewFriend);
-
-app.mount("#app");
+createApp(App).mount('#app');
