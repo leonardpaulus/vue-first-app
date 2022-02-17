b8a792aff50487dd60750309426be8b3df639922
leonardpaulus <leonard.paulus@agenturignis.de>
2022-02-17 08:36:29 +0100

    ✨ Add routes

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,46 @@
 <template>
-  <section>
-    <header><h1>My Friends</h1></header>
-    <new-friend @post-friend="addFriendToFriends"></new-friend>
-    <ul>
-      <friend-contact
-        v-for="friend in friends"
-        :key="friend.id"
-        :id="friend.id"
-        :name="friend.name"
-        :phone-number="friend.phone"
-        :email-adress="friend.email"
-        :is-favorite="friend.isFavorite"
-        @toggle-favorite="toggleFavoriteStatus"
-        @delete-friend="deleteFriend"
-      ></friend-contact>
-    </ul>
-  </section>
+  <the-navigation @set-page="setActivePage"></the-navigation>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer>
+    <router-view name="footer"></router-view>
+  </footer>
 </template>
 
 <script>
+import TheNavigation from "./components/nav/TheNavigation.vue";
+
 export default {
+  components: {
+    TheNavigation,
+  },
   data() {
     return {
-      detailsAreVisible: false,
-      friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "12312412341",
-          email: "manuel@localhost.com",
-          isFavorite: true,
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "8932419123",
-          email: "julie@localhost.com",
-          isFavorite: false,
-        },
+      activePage: "teams-list",
+      teams: [
+        { id: "t1", name: "Frontend Engineers", members: ["u1", "u2"] },
+        { id: "t2", name: "Backend Engineers", members: ["u1", "u2", "u3"] },
+        { id: "t3", name: "Client Consulting", members: ["u4", "u5"] },
+      ],
+      users: [
+        { id: "u1", fullName: "Max Schwarz", role: "Engineer" },
+        { id: "u2", fullName: "Praveen Kumar", role: "Engineer" },
+        { id: "u3", fullName: "Julie Jones", role: "Engineer" },
+        { id: "u4", fullName: "Alex Blackfield", role: "Consultant" },
+        { id: "u5", fullName: "Marie Smith", role: "Consultant" },
       ],
     };
   },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
+  },
   methods: {
-    toggleFavoriteStatus(friendId) {
-      const identifiedFriend = this.friends.find(
-        (friend) => friend.id === friendId
-      );
-      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
-    },
-    addFriendToFriends(friend) {
-      this.friends.push(friend);
-    },
-    deleteFriend(friendId) {
-      this.friends = this.friends.filter((friend) => friend.id !== friendId);
+    setActivePage(page) {
+      this.activePage = page;
     },
   },
 };
@@ -64,67 +52,10 @@ export default {
 }
 
 html {
-  font-family: "Jost", sans-serif;
+  font-family: sans-serif;
 }
 
 body {
   margin: 0;
-}
-
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-#app li {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-
-#app button,
-.button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-.buttons {
-  display: flex;
-  justify-content: space-evenly;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,65 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
 import App from "./App.vue";
-import FriendContact from "./components/FriendContact.vue";
-import NewFriend from "./components/NewFriend.vue";
+import TeamsList from "./components/teams/TeamsList.vue";
+import UsersList from "./components/users/UsersList.vue";
+import TeamMembers from "./components/teams/TeamMembers.vue";
+import NotFound from "./components/nav/NotFound.vue";
+import TeamsFooter from "./components/teams/TeamsFooter.vue";
+import UsersFooter from "./components/users/UsersFooter.vue";
 
-const app = createApp(App);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/teams" },
+    {
+      name: "teams",
+      path: "/teams",
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: "team-members",
+          path: ":teamId",
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
+    {
+      path: "/users",
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        console.log(to, from);
+        next();
+      },
+    },
 
-app.component("friend-contact", FriendContact);
-app.component("new-friend", NewFriend);
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+  linkActiveClass: "active",
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
+    else
+      return {
+        left: 0,
+        top: 0,
+      };
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  console.log("Global beforeEach: ");
+  console.log(to, from);
+  //   if (to.name === "team-members") {
+  //     next();
+  //   } else {
+  //     next({ name: "team-members", params: { teamId: "t2" } });
+  //   }
+  next();
+});
+
+const app = createApp(App).use(router);
 
 app.mount("#app");
